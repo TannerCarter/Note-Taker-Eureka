e3e5f320aa7a556378ac23824733e6adbef96624
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-04 15:21:54 -0700

    htmlRoute is complete, GET * directs to index.html and GET /notes returns notes.html

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 const app = express();
 const fs = require("fs");
 
 //Setting these variables for future use of routes
-const apiRoutes = require("./routes/htmlRoutes/apiRoutes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 //Include Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //app.use routes will go here
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //Make server listen
 app.listen(PORT, () => {
