c8c3f6a7b15066d93ccc4e9b27f461ae13ee727a
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-04 14:10:03 -0700

    Setting up routes to use, structure based from module

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require("fs");
+
+//Setting these variables for future use of routes
+const apiRoutes = require("./routes/htmlRoutes/apiRoutes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes/htmlRoutes");
+
+//Include Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//app.use routes will go here
 
 //Make server listen
-app.listen(3001, () => {
-  console.log(`API server is active on port 3001!`);
+app.listen(PORT, () => {
+  console.log("API server is now on port ${PORT}!");
 });
