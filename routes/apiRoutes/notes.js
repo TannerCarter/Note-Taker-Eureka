168daa6f82a6b63d2017d909b128968c76778cd6
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-07-04 16:00:33 -0700

    Write to db.json, generate id using nanoid, ability to delete note, and rewrite to db.json

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,3 +1,57 @@
 const router = require("express").Router();
+const fs = require("fs");
+const { db } = require("../../db/db.json");
+
+//include nanoid
+const { nanoid } = require("nanoid");
+// Use fs to pull notes from db.json
+const savedNotes = fs.readFileSync("./db/db.json");
+if (savedNotes) {
+  let invalid = JSON.parse(savedNotes);
+  notes = invalid;
+} else {
+  notes = [];
+}
+
+router.get("/notes", (req, res) => {
+  return res.json(notes);
+});
+//Use nanoid to generate ids - Saved note to show in console -
+router.post("/notes", function (req, res) {
+  let validId = nanoid();
+
+  let validNote = {
+    id: validId,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(validNote);
+  notes.push(validNote);
+  //Send validNote back
+  res.json(validNote);
+  fs.writeFileSync(
+    "./db/db.json",
+    JSON.stringify(notes, null, 2),
+    function (err) {
+      if (err) throw err;
+    }
+  );
+});
+
+//Delete note
+router.delete("/notes/:id", (req, res) => {
+  let deleteNote = notes.findIndex((item) => item.id === req.params.id);
+  notes.splice(deleteNote, 1);
+
+  //Write new
+  fs.writeFileSync(
+    "./db/db.json",
+    JSON.stringify(notes, null, 2),
+    function (err) {
+      if (err) throw err;
+    }
+  );
+  res.json({ deletion: "Your note has been succesfully deleted" });
+});
 
 module.exports = router;
